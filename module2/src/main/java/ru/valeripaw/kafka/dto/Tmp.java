a2a32170ee91996c3424190643d7fc00666359ba
valeri <valeria.pokrovskaya@scentbird.com>
2026-01-21 19:40:27 +0400

    module 2

---
+++ module2/src/main/java/ru/valeripaw/kafka/dto/Tmp.java
@@ -0,0 +1,18 @@
+package ru.valeripaw.kafka.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Tmp {
+
+    private Message message;
+    private String blockedUser;
+}
